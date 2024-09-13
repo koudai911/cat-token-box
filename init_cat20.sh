a703f89e74b4fcd6768c1f4c94d77f0504436e6b
koudai <46878560+koudai911@users.noreply.github.com>
2024-09-13 13:39:23 +0800

    Update init_cat20.sh

--- init_cat20.sh
+++ init_cat20.sh
@@ -1,4 +1,4 @@
-TRACKER_IP="$1"
+
 cd ~
 git clone https://github.com/koudai911/cat-token-box.git
 cd cat-token-box
@@ -8,20 +8,6 @@ sudo chmod 777 packages/tracker/docker/data
 sudo chmod 777 packages/tracker/docker/pgdata
 
 cd packages/cli 
-
-echo '{
-      "network": "fractal-mainnet",
-      "tracker": "http:// $TRACKER_IP :3000",
-      "dataDir": ".",
-      "maxFeeRate": 30,
-      "rpc": {
-          "url": "http:// $TRACKER_IP :8332",
-          "username": "bitcoin",
-          "password": "opcatAwesome"
-      }
-    }' > ~/cat-token-box/packages/cli/config.json
-
-
 yarn cli wallet create
 chmod +x script.sh
 yarn cli wallet address
