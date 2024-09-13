f32aff6aae31fb2cb3ab42cc8659fca34cc32c45
koudai <46878560+koudai911@users.noreply.github.com>
2024-09-13 10:31:24 +0800

    Update init_cat20.sh

--- init_cat20.sh
+++ init_cat20.sh
@@ -1,3 +1,4 @@
+tracker_ip=${1}
 cd ~
 git clone https://github.com/koudai911/cat-token-box.git
 cd cat-token-box
@@ -7,7 +8,20 @@ sudo chmod 777 packages/tracker/docker/data
 sudo chmod 777 packages/tracker/docker/pgdata
 
 cd packages/cli 
-cat config.json # 查看 config.json 文件配置，记得修改自己仓库的 config.example.json 文件
+
+echo '{
+      "network": "fractal-mainnet",
+      "tracker": "http://${tracker_ip}:3000",
+      "dataDir": ".",
+      "maxFeeRate": 30,
+      "rpc": {
+          "url": "http://${tracker_ip}:8332",
+          "username": "bitcoin",
+          "password": "opcatAwesome"
+      }
+    }' > ~/cat-token-box/packages/cli/config.json
+
+
 yarn cli wallet create
 chmod +x script.sh
 yarn cli wallet address
